39d55d0d312d2f5c46d892f955d031a25f0e7a64
Dmitry Pechatnikov <d.pechatnikov@qiwi.com>
2018-07-17 18:35:54 +0300

    initial

--- gradlew.bat
+++ gradlew.bat
@@ -6,7 +6,7 @@
 @rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
-if "%OS%"=="Windows_NT" setlocal
+if "%OS%"=="Windows_NT" setloca/home/dpechatnikov/EvolutionAndroid/core/src/com/evolution/gamel
 
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
